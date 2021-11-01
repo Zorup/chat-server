b6d4a5711ac36bc2f84b07123fc429d5534770d2
lophunju <lophunju@naver.com>
2021-11-02 04:12:40 +0900

    [기능추가] 채팅방 삭제 API

--- src/main/java/com/zorup/chat/controller/OpenRoomController.java
+++ src/main/java/com/zorup/chat/controller/OpenRoomController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import com.zorup.chat.service.OpenRoomService;
 import com.zorup.chat.dto.OpenRoomDto;
@@ -25,6 +26,12 @@ public class OpenRoomController {
     @PostMapping(value="/room")
     public OpenRoomDto addOpenRoom(@RequestBody OpenRoomDto openRoomDto){
         return openRoomService.insertOpenRoom(openRoomDto);
+    }
+
+    @DeleteMapping(value="/{userId}/room/{roomId}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteOpenRoom(@PathVariable("userId") Long userId, @PathVariable("roomId") String roomId){
+        openRoomService.deleteOpenRoom(userId, roomId);
     }
 
     @PutMapping(value="/room")
